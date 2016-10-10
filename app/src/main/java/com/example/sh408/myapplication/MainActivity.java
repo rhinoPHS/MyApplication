5db8ab6546a4250b09721c2aaf46510ecb25addc
phs <cord7894@naver.com>
2016-10-10 21:07:33 +0900

    20161010

--- app/src/main/java/com/example/sh408/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sh408/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sh408.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v)
+    {
+        //Toast.makeText(getApplicationContext(),"버튼이 눌러졌습니다",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(MainActivity.this,"버튼이 눌러졌습니다",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"버튼이 눌러졌습니다",Toast.LENGTH_SHORT).show();
+    }
+
 }
